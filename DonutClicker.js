4f9f3e195e2177006fd644b47f511adf38ea5f79
JNegron300 <metal_x40@live.com>
2022-07-09 09:25:01 -0400

    push

---
+++ DonutClicker.js
@@ -0,0 +1,33 @@
+// import DonutClicker from "./index";
+
+// function Click() {
+//     document.getElementById("pic").addEventListener("click", some_action)
+
+
+//     let x = 0
+//     document.getElementById(".donutCounter'").innerHTML = x
+//     document.getElementById('pic').addEventListener("click", iterate)
+//     function iterate(){
+//         x += 1
+//         console.log(x)
+//         document.getElementById(".donutCounter'").innerHTML = x
+//     }
+// }
+// showNumberDonuts();
+// let donutCounter = document.querySelector('.donutCounter');
+// let donutDisplay = document.querySelector('.donutDisplay');
+// donutCounter.addEventListener("click",()=>{
+//     Clicks++;
+//     showNumberDonuts();
+// }) ;
+// function showNumberDonuts(){
+//     donutDisplay.innerHTML = Clicks;
+// };
+
+// export default DonutClicker;
+
+
+var i = 100
+for (; i < 1000000000; i *=2){
+    console.log(i);
+}
